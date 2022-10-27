e8f830eea1cda90d86cac983540bfb33d42c42da
Le Tho <tho.le@workbuddy.com>
2022-10-27 22:57:20 +0700

    prepare for build

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
