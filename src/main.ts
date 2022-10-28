59efa1261e3dfe1ac753baa1918c2b44c26eb047
Le Tho <tho.le@workbuddy.com>
2022-10-28 09:13:27 +0700

    fix config

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
