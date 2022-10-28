8ffaf6b0e0ebf33907afb0eb23b4107d5433c3c4
Le Tho <tho.le@workbuddy.com>
2022-10-28 09:44:51 +0700

    trigger deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const msg = process.env.MSG || 'wow it works 001';
+    const msg = process.env.MSG || 'wow it works 004';
     return `Hello World From NestJS! - ${msg}`;
   }
 }
